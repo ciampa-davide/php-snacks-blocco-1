d3bd299bb0e600aeb216c2e10bfbef579d74a704
Davide <ciampad06@gmail.com>
2021-01-20 17:44:53 +0100

    fine secondo snack

--- PHP-snack-2.php
+++ PHP-snack-2.php
@@ -11,21 +11,21 @@ Se tutto è ok stampare “Accesso riuscito”, altrimenti
   $name = $_GET["name"];
   $email = $_GET["mail"];
   $age = $_GET["age"];
-  $check = is_numeric($age);
+
 
 
   //controllo sulla lunghezza del nome
     if(strlen($name) > 3){
 
       if(strpos($email, ".") !== false && strpos($email, "@") !== false ){
-        $access = "Accesso consentito";
-        // if(is_int($age)){
-        //
-        //   $access = "Accesso consentito";
-        //
-        // }else{
-        //   $access = "Accesso Negato"
-        // }
+
+        if(is_numeric($age) !== false){
+
+          $access = "Accesso consentito";
+
+        }else{
+          $access = "Accesso Negato";
+        }
 
       }else{
         $access = "Accesso Negato";
@@ -46,7 +46,9 @@ Se tutto è ok stampare “Accesso riuscito”, altrimenti
   <title>Snack 2</title>
 </head>
 <body>
-   <p><?php echo $access ?></p>
-   <p><?php echo var_dumb($chek) ?></p>
+  <p><?php echo $name ?></p>
+  <p><?php echo $email ?></p>
+  <p><?php echo $age ?></p>
+  <p><?php echo $access ?></p>
 </body>
 </html>
